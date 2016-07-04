838f6584ec500f07cb27e3691979aea8301fafe8
sujit <seelasujit77@gmail.com>
2016-07-04 13:52:33 +0530

    Second commit

--- GitPractice/app/src/main/java/com/practice/sujit/gitpractice/MainActivity.java
+++ GitPractice/app/src/main/java/com/practice/sujit/gitpractice/MainActivity.java
@@ -2,12 +2,15 @@ package com.practice.sujit.gitpractice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv=(TextView)findViewById(R.id.idtv);
+        tv.setText("sujit is here ");
     }
 }
